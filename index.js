35651b2388cf392adb692b343dd442b6dc27d20a
JongHoonC <bjhlove0126@naver.com>
2023-01-06 11:39:35 +0900

    asdfasdf

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
-function addInput(num) {
-  // 숫자나 기호가 불려지면 그대로 화면에 출력
+function number(num) {
   let viewScreen = document.getElementById("viewScreen");
   if (viewScreen.value == "0") {
-    // 단순 초기값인 경우
     viewScreen.value = num;
   } else {
-    viewScreen.value += num; // 출력창에 버튼의 문자 덧붙임
+    viewScreen.value += num;
   }
 }
 
-function backward() {
+function del() {
   let viewScreen = document.getElementById("viewScreen");
   let length = viewScreen.value.length;
-  if (length == 0) {
+  if (length === 0) {
     return;
   } else {
     viewScreen.value = viewScreen.value.substr(0, length - 1);
   }
 }
+
+// 계산
 function calculate() {
   let viewScreen = document.getElementById("viewScreen");
   viewScreen.value = eval(viewScreen.value);
 }
+
+// clear
 function clearviewScreen() {
   let viewScreen = document.getElementById("viewScreen");
   viewScreen.value = "0";
